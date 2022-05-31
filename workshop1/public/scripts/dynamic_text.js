a4e7dffdcc29a1bb3b4a412136f51490f514c460
vios484 <82788760+vios484@users.noreply.github.com>
2022-05-31 18:54:06 +0800

    Update dynamic_text.js

--- workshop1/public/scripts/dynamic_text.js
+++ workshop1/public/scripts/dynamic_text.js
@@ -1,11 +1,11 @@
-var myQuotes = new Array();
+var FavQuotes = new Array();
 
-myQuotes[0] = "Logic will get you form A to B. Imagination will take you everywhere.";
-myQuotes[1] = "There are 10 kinds of people. Those who know binary and those who don't.";
-myQuotes[2] = "There are two ways of constructing a software design. One way is to make it so simple that there are obviously no deficiencies and the other is to make it so complicated that thereare no obvious deficiencies.";
-myQuotes[3] = "It's not that I'm so smart, it's just that I stay with problems longer.";
-myQuotes[4] = "It is pitch dark. You are likely to be eaten by a grue.";
+FavQuotes[0] = "Logic will get you form A to B. Imagination will take you everywhere.";
+FavQuotes[1] = "There are 10 kinds of people. Those who know binary and those who don't.";
+FavQuotes[2] = "There are two ways of constructing a software design. One way is to make it so simple that there are obviously no deficiencies and the other is to make it so complicated that thereare no obvious deficiencies.";
+FavQuotes[3] = "It's not that I'm so smart, it's just that I stay with problems longer.";
+FavQuotes[4] = "It is pitch dark. You are likely to be eaten by a grue.";
 
-var myRandom = Math.floor(Math.random()*myQuotes.length);
+var FavRandom = Math.random()%5;
 
-$('#myQuote').html(myQuotes[myRandom]);
+$('#FavQuote').html(FavQuotes[FavRandom]);
