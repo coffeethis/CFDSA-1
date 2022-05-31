e1a9d270274514274c55ca03a96a0f5a16489e16
vios484 <82788760+vios484@users.noreply.github.com>
2022-05-31 19:08:37 +0800

    Modified

--- workshop1/public/scripts/dynamic_text.js
+++ workshop1/public/scripts/dynamic_text.js
@@ -6,6 +6,6 @@ FavQuotes[2] = "There are two ways of constructing a software design. One way is
 FavQuotes[3] = "It's not that I'm so smart, it's just that I stay with problems longer.";
 FavQuotes[4] = "It is pitch dark. You are likely to be eaten by a grue.";
 
-var FavRandom = Math.random()%5;
+var FavRandom = Math.floor(Math.random()*FavQuotes.length);;
 
 $('#FavQuote').html(FavQuotes[FavRandom]);
